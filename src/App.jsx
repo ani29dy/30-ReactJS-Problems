f5d1c168c5696ba9a02d31de44f10aca0f891fca
ani29dy <aniketanni2916@gmail.com>
2025-02-02 19:56:03 +0530

    Rendered Problem Three

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import One from "./Problems/One";
+import Three from "./Problems/Three";
 import Two from "./Problems/Two";
 
 function App() {
   return (
     <>
       {/* <One /> */}
-      <Two />
+      {/* <Two /> */}
+      <Three />
     </>
   );
 }
