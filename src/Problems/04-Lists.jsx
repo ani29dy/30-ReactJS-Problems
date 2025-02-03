96acfe67099e5b01adcda8da7c7881c20b64c53e
ani29dy <aniketanni2916@gmail.com>
2025-02-03 21:37:56 +0530

    Problem Four Solved on Lists

---
+++ src/Problems/04-Lists.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Lists = () => {
+  return (
+    <div>
+      <div>
+        Ordered Lists
+        <ol>
+          <li>Item-1</li>
+          <li>Item-2</li>
+          <li>Item-3</li>
+          <li>Item-4</li>
+          <li>Item-5</li>
+        </ol>
+      </div>
+      <div>
+        Unordered Lists
+        <ul>
+          <li>Item-1</li>
+          <li>Item-2</li>
+          <li>Item-3</li>
+          <li>Item-4</li>
+          <li>Item-5</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Lists;
