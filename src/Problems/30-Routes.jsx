20fe7ac2eff93ae7178de96fdbb7337e1e5bf2a2
ani29dy <aniketanni2916@gmail.com>
2025-10-31 19:16:08 +0530

    Problem 30 solved on Multiple Routes Navigating

---
+++ src/Problems/30-Routes.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Routes/Home";
+import About from "./Routes/About";
+import Contact from "./Routes/Contact";
+import Themes from "./Routes/Themes";
+
+const RoutesExample = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/themes" element={<Themes />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default RoutesExample;
