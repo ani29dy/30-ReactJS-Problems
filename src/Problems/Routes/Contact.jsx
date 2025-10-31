dd6e7b9cff433afb41c8ee786ab298977a28a359
ani29dy <aniketanni2916@gmail.com>
2025-10-31 19:17:47 +0530

    Contact Page for Problem 30

---
+++ src/Problems/Routes/Contact.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+
+const Contact = () => {
+  useEffect(() => {
+    document.title = "CONTACT";
+  });
+
+  return (
+    <div>
+      <h1>Contact</h1>
+    </div>
+  );
+};
+
+export default Contact;
